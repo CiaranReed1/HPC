3f19bc6f2014f6dc4b70629e9c7dc25639a9b264
Ciaran Reed <ciaranreed@hotmail.co.uk>
2025-11-18 10:51:32 +0000

    implemented encoder.c

--- openmp-descrambling/encoder.c
+++ openmp-descrambling/encoder.c
@@ -1,12 +1,12 @@
 #include <stdio.h>  // Import the printf function
 #include <omp.h>    // Import the OpenMP library functions
+#include <string.h>
 
 // Start of program
 int main(){
 
    // This is a "character array", consisting of 105 letters in byte form.
    char mymessage[] = "This is a placeholder of the original message";
-
    // The message is encoded by shifting all letters by one, i.e. 
    // A becomes B, c becomes d and so forth. This shift is easy to implement
    // in byte form as all you need to do is to add 1 to a character.
@@ -16,7 +16,10 @@ int main(){
    //       printf("%s\n", mymessage);
 
    /* MISSING CODE: for loop over the 105 characters and adding the value 1 to each entry; */
-    
+   int length = strlen(mymessage);
+   for (int i = 0; i < length; i++){
+      mymessage[i] = mymessage[i] + 1;
+   }
    printf("%s\n", mymessage); // This prints a character array to screen
 
    return 0; // Exit program

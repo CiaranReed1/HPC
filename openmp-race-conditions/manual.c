c7cc040adc40d99a608d6b1c75ce344323953340
Ciaran Reed <ciaranreed@hotmail.co.uk>
2025-11-20 14:20:10 +0000

    fixed to actually execute parallel region

--- openmp-race-conditions/manual.c
+++ openmp-race-conditions/manual.c
@@ -5,14 +5,16 @@ int main(){
     int sum =0;
     int _sum;
     #pragma omp parallel num_threads(1) default(none) shared(N,sum) private(_sum)
-    _sum = 0;
-    #pragma omp for
-    for (int n=1; n<N+1; n++){
-        _sum+=n;
-    }
-    #pragma omp critical 
     {
-        sum += _sum;
+        _sum = 0;
+        #pragma omp for
+        for (int n=1; n<N+1; n++){
+            _sum+=n;
+        }
+        #pragma omp critical 
+        {
+            sum += _sum;
+        }
     }
     printf("Result is %d. It should be %d.\n",
         sum, N*(N+1)/2);

51e190b393f2af4dde0e6ab21ef9e2ba8233043e
Ciaran Reed <ciaranreed@hotmail.co.uk>
2025-11-26 17:34:58 +0000

    changed python to output 2 figures, updated slurm to explicitly run 2 tasks per node

--- mpi-ping-pong/fit.py
+++ mpi-ping-pong/fit.py
@@ -3,18 +3,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy import stats
 data_1_node = pd.read_csv('one-node-pp.dat', delimiter=",", header=None, names=['iterations', 'nbytes','time'])
-print(data_1_node)
+data_2_node = pd.read_csv('two-node-pp.dat', delimiter=",", header=None, names=['iterations', 'nbytes','time'])
 fig, axs = plt.subplots(1, 2, figsize=(15, 6))
-axs[0].plot(data_1_node['nbytes'], data_1_node['time']/data_1_node['iterations'], marker='x', label='1 Node',color = "blue",linestyle="")
-xs = np.linspace(data_1_node['nbytes'].min(), data_1_node['nbytes'].max(), 10000)
-fit = stats.linregress(data_1_node['nbytes'], data_1_node['time']/data_1_node['iterations'])
-alpha = fit.intercept
-beta = fit.slope
-print(alpha)
-axs[0].plot(xs, alpha + beta*xs,marker = "", color='red')
-axs[0].set_xlabel('Message Size (bytes)')
-axs[0].set_ylabel('Time per Ping-Pong (seconds)')
-axs[0].set_title('(Same Node), latency = %.2e s, inverse bandwidth = %.2e s/B' % (alpha, beta))
-axs[0].legend()
-fig.savefig('ping_pong_same_node.png')
+for i, data in enumerate([data_1_node, data_2_node]):
+    axs[i].plot(data['nbytes'], data['time']/data['iterations'], marker='x', label=f'{i+1} Node{"s" if i else ""}',color = "blue",linestyle="")
+    xs = np.linspace(data['nbytes'].min(), data['nbytes'].max(), 10000)
+    fit = stats.linregress(data['nbytes'], data['time']/data['iterations'])
+    alpha = fit.intercept
+    beta = fit.slope
+    axs[i].plot(xs, alpha + beta*xs,marker = "", color='red')
+    axs[i].set_xlabel('Message Size (bytes)')
+    axs[i].set_ylabel('Time per Ping-Pong (seconds)')
+    axs[i].set_title(f'({"Same" if i == 0 else "Different"} Node), latency = %.2e s, inverse bandwidth = %.2e s/B' % (alpha, beta))
+    axs[i].legend()
+fig.savefig('ping_pong.png')
 plt.show()

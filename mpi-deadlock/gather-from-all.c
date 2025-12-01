b500096c9173040bf2c8bda69796cd823ac7ff2e
Ciaran Reed <ciaranreed@hotmail.co.uk>
2025-12-01 13:53:14 +0000

    added non blocking gather and binary

--- mpi-deadlock/gather-from-all.c
+++ mpi-deadlock/gather-from-all.c
@@ -7,12 +7,17 @@ void gather_nonblocking(const int *send, int *recvbuf, MPI_Comm comm)
   int size, rank;
   MPI_Comm_size(comm, &size);
   MPI_Comm_rank(comm, &rank);
+  
 
-  if (rank == 0) {
-   //??
-  } else {
-   //??
-  }
+	if (rank == 0) {
+		MPI_Request requests[size-1];
+		for (int i =1;i < size;i++){
+			MPI_Irecv(recvbuf+i,1,MPI_INT,MPI_ANY_SOURCE,0,comm,&requests[i-1]);
+		}
+		MPI_Waitall(size-1, requests, MPI_STATUS_IGNORE);
+	} else {
+		MPI_Send(send,1,MPI_INT,0,0,comm);
+	}
 }
 
 void gather_blocking(const int *send, int *recvbuf, MPI_Comm comm)
@@ -67,14 +72,15 @@ int main(int argc, char **argv){
 	if (rank == 0) {
 		printf("Blocking gather takes %.3g s\n", (end - start)/100);
 	}
-	// start = MPI_Wtime();
-	// for (int i = 0; i < 100; i++) {
-	// 	gather_nonblocking(&local, nonblocking, comm);
-	// }
-	// end = MPI_Wtime();
-	// if (rank == 0) {
-	// 	printf("Non-blocking gather takes %.3g s\n", (end - start)/100);
-	// }
+	start = MPI_Wtime();
+	for (int i = 0; i < 100; i++) {
+		gather_nonblocking(&local, nonblocking, comm);
+	}
+	end = MPI_Wtime();
+	if (rank == 0) {
+		printf("Non-blocking gather takes %.3g s\n", (end - start)/100);
+	}
+
 	free(blocking);
 	free(nonblocking);
 	MPI_Finalize();

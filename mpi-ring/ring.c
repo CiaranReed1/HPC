2ab5f8537ab169fb804f92cf64d67f69b35862dc
Ciaran Reed <ciaranreed@hotmail.co.uk>
2025-12-01 16:32:37 +0000

    added ring functionality for p processess

--- mpi-ring/ring.c
+++ mpi-ring/ring.c
@@ -7,9 +7,12 @@ int main(int argc, char **argv){
 	MPI_Comm comm;
 	comm = MPI_COMM_WORLD;
 
-	int rank, size;
+	int rank, size,left,right;
 	MPI_Comm_rank(comm, &rank);
 	MPI_Comm_size(comm, &size);
+
+	left = (rank - 1 + size) % size;
+	right = (rank + 1) % size;
 
 	int local_value = rank;
 	int summed_value = 0;	// initialized sum: 0
@@ -17,9 +20,14 @@ int main(int argc, char **argv){
 	printf("[%d] Before reduction: local value is %d; summed value is %d\n",
 	      rank, local_value, summed_value);
 	double start = MPI_Wtime();
-	/*
-	// Implement the ring-reduction here
-	*/
+	summed_value = local_value;
+    for(int i=0; i <size -1;i++){
+		MPI_Request request;
+		MPI_Isend(&local_value,1,MPI_INT,right,0,comm,&request);
+		MPI_Recv(&local_value,1,MPI_INT,left,0,comm,MPI_STATUS_IGNORE);
+		summed_value+=local_value;
+		MPI_Wait(&request,MPI_STATUS_IGNORE);
+	}
 	double end = MPI_Wtime();
 	printf("[%d] After reduction: local value is %d; summed value is %d (should be %d)\n",
 	      rank, local_value, summed_value, size*(size-1)/2);

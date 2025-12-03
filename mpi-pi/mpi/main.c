5c07ef3d8134b31b972a2a5b9d302e08c4a76710
CiaranReed1 <ciaranreed@hotmail.co.uk>
2025-12-03 15:39:02 +0000

    hamilton strong scaling

--- mpi-pi/mpi/main.c
+++ mpi-pi/mpi/main.c
@@ -24,7 +24,7 @@ int main(int argc, char *argv[]) {
   int size, rank;
   MPI_Comm_size(MPI_COMM_WORLD, &size);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  printf("Hello from process %d of %d\n", rank, size);
+  //printf("Hello from process %d of %d\n", rank, size);
 
   double start, end;
 

eba76225c92fa87234095be945ce731ece19d17f
Ciaran Reed <ciaranreed@hotmail.co.uk>
2025-12-03 12:16:57 +0000

    serial run script and data

--- mpi-pi/serial/run-serial.sh
+++ mpi-pi/serial/run-serial.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 make 
 echo "N,calculated_pi,error,time" > serial.dat
-for i in 10 100 1000 10000 100000 1000000 10000000; do
+for i in $(seq 10 1 5000); do
     echo "Running with input $i"
     ./calcpi "$i" >> serial.dat
 done
 make clean
+python3 serial.py

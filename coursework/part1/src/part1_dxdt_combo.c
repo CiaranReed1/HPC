c1fd25ef6cb68713c52e0ef8b302e103d8b8ab56
Ciaran Reed <ciaranreed@hotmail.co.uk>
2025-12-22 22:01:28 +0000

    source file with the best implementation of each function. and combo for dxdt

--- coursework/part1/src/part1_dxdt_combo.c
+++ coursework/part1/src/part1_dxdt_combo.c
@@ -15,23 +15,24 @@ However this requires doing two divisions every iteration so this may be detrime
 */
 void init(double *u, double *v) {
   int idx,i,j;
-  #pragma omp parallel for default(none) shared(u,v,N,M,uhi,ulo,vhi,vlo) private(idx,i,j) collapse(2)
-  for (i = 0;i<M;i++){
-    for (j = 0;j<N;j++){
+  for (i = 0; i< M; i++) {
+    for (j = 0; j < N; j++) {
       idx = i * N + j;
+     //initialize u and v with smooth gradients in both directions
       u[idx] = ulo + (uhi - ulo) * 0.5 * (1.0 + tanh((i - M / 2) / 16.0)); //smooth gradient in vertical direction
       v[idx] = vlo + (vhi - vlo) * 0.5 * (1.0 + tanh((j - N / 2) / 16.0)); //smooth gradient in horizontal direction
     }
   }
 }
+
 void dxdt(double *du, double *dv, const double *u, const double *v) {
   double lapu, lapv, _u, _v;
   int up, down, left, right, idx,i,j; // indicies of neighboring grid positions
   #pragma omp parallel default(none) shared(u,v,du,dv,M,N,DD,R,d) private(idx,i,j,up,down,left,right,lapu,lapv,_u,_v)
   {
-    #pragma omp single nowait//left and right edges (excluding corners)
+    #pragma omp single nowait
     {
-      for (i = 1; i < M-1; i++) { 
+      for (i = 1; i < M-1; i++) { //left and right edges (excluding corners)
         //start with left edge : j = 0
         up = (i + 1) * N;
         down = (i - 1) * N;
@@ -59,9 +60,9 @@ void dxdt(double *du, double *dv, const double *u, const double *v) {
         dv[idx] = d * DD * lapv + g(_u, _v);
       }
     }
-    #pragma omp single nowait //bottom and top edges
+    #pragma omp single nowait
     {
-      for (j = 0; j < N; j++) { 
+      for (j = 0; j < N; j++) { //bottom and top edges
         //start with bottom edge : i = 0
         up = N + j;
         down = j;
@@ -107,39 +108,38 @@ void dxdt(double *du, double *dv, const double *u, const double *v) {
     }
   }
   }
-//should be straight forward to parallelise as there are no data dependencies
-//again similar to init, this could be collapsed but likely not worth it
-//also similar to init, i could condense this into one loop over the range of idx 
 
-/*unlike init, each step here does not require calculating i and j from idx 
-and so i believe this may yield performace gains*/
-void step(const double *du, const double *dv, double *u, double *v) {
-  int idx,i,j;
-  for (i = 0; i < M; i++) {
-    for (j = 0; j < N; j++) {
-      idx = i * N + j;
-     //for every grid point update u and v
-      u[idx] += dt * du[idx];
-      v[idx] += dt * dv[idx];
+  //should be straight forward to parallelise as there are no data dependencies
+  //again similar to init, this could be collapsed but likely not worth it
+  //also similar to init, i could condense this into one loop over the range of idx 
+
+  /*unlike init, each step here does not require calculating i and j from idx 
+  and so i believe this may yield performace gains*/
+  void step(const double *du, const double *dv, double *u, double *v) {
+    int idx,i,j;
+    for (i = 0; i < M; i++) {
+      for (j = 0; j < N; j++) {
+        idx = i * N + j;
+      //for every grid point update u and v
+        u[idx] += dt * du[idx];
+        v[idx] += dt * dv[idx];
+      }
     }
   }
-}
-// calculate the norm of a 2D field stored in a 1D array
-// this can be parallellised using a reduction clause
-// this also might be worth condensing into a single loop over idx, similar to init and step
-double norm(const double *x) {
-  double nrmx = 0.0;
-  int idx,i,j;
-  #pragma omp parallel for default(none) shared(x,M,N) private(idx,i,j) reduction(+:nrmx)
-  for (i = 0; i < M; i++) {
-    for (j = 0; j < N; j++) {
-      idx = i * N + j;
-      nrmx += x[idx] * x[idx];
-    }
+  // calculate the norm of a 2D field stored in a 1D array
+  // this can be parallellised using a reduction clause
+  // this also might be worth condensing into a single loop over idx, similar to init and step
+  double norm(const double *x) {
+    double nrmx = 0.0;
+    int idx,i,j;
+    for (i = 0; i < M; i++) {
+      for (j = 0; j < N; j++) {
+        idx = i * N + j;
+        nrmx += x[idx] * x[idx];
+      }
   }
   return nrmx;
 }
-
 
 int main(int argc, char **argv) {
   double start,end;
@@ -181,8 +181,8 @@ int main(int argc, char **argv) {
     }
   }
   // write norms output
-  char filename[30];
-  sprintf(filename, "%d_cores_part1.dat", omp_get_max_threads());
+  char filename[50];
+  sprintf(filename, "%d_cores_part1_dxdt_combo.dat", omp_get_max_threads());
   FILE *fptr = fopen(filename, "w");
   fprintf(fptr, "#t\t\tnrmu\t\tnrmv\n");
   for (int k = 0; k < (T / m); k++) {

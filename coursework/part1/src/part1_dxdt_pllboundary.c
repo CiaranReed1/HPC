3b65cd76be23be162e414765646317575a90c1c5
Ciaran Reed <ciaranreed@hotmail.co.uk>
2025-12-23 14:57:18 +0000

    new test for parallelised the boundary points with nowait, main c file now uses one parallel region within main

--- coursework/part1/src/part1_dxdt_pllboundary.c
+++ coursework/part1/src/part1_dxdt_pllboundary.c
@@ -15,21 +15,23 @@ However this requires doing two divisions every iteration so this may be detrime
 */
 void init(double *u, double *v) {
   int idx,i,j;
-  #pragma omp parallel for default(none) shared(u,v,N,M,uhi,ulo,vhi,vlo) private(idx,i,j) collapse(2)
-  for (i = 0;i<M;i++){
-    for (j = 0;j<N;j++){
+  for (i = 0; i< M; i++) {
+    for (j = 0; j < N; j++) {
       idx = i * N + j;
+     //initialize u and v with smooth gradients in both directions
       u[idx] = ulo + (uhi - ulo) * 0.5 * (1.0 + tanh((i - M / 2) / 16.0)); //smooth gradient in vertical direction
       v[idx] = vlo + (vhi - vlo) * 0.5 * (1.0 + tanh((j - N / 2) / 16.0)); //smooth gradient in horizontal direction
     }
   }
 }
+
 void dxdt(double *du, double *dv, const double *u, const double *v) {
   double lapu, lapv, _u, _v;
   int up, down, left, right, idx,i,j; // indicies of neighboring grid positions
-  #pragma omp single nowait private(i,j,up,down,left,right,idx,_u,_v,lapu,lapv)//left and right edges (excluding corners)
+  #pragma omp parallel default(none) shared(u,v,du,dv,M,N,DD,R,d) private(idx,i,j,up,down,left,right,lapu,lapv,_u,_v)
   {
-    for (i = 1; i < M-1; i++) { 
+    #pragma omp for nowait 
+    for (i = 1; i < M-1; i++) { //left and right edges (excluding corners)
       //start with left edge : j = 0
       up = (i + 1) * N;
       down = (i - 1) * N;
@@ -55,11 +57,9 @@ void dxdt(double *du, double *dv, const double *u, const double *v) {
       lapv = v[up] + v[down] + v[left] + v[right] + -4.0 * _v;
       du[idx] = DD * lapu + f(_u, _v) + R * stim(i, j); 
       dv[idx] = d * DD * lapv + g(_u, _v);
-    }
-  }
-  #pragma omp single nowait private(i,j,up,down,left,right,idx,_u,_v,lapu,lapv) //bottom and top edges
-  {
-    for (j = 0; j < N; j++) { 
+   }
+    #pragma omp for nowait
+    for (j = 0; j < N; j++) { //bottom and top edges
       //start with bottom edge : i = 0
       up = N + j;
       down = j;
@@ -86,91 +86,86 @@ void dxdt(double *du, double *dv, const double *u, const double *v) {
       du[idx] = DD * lapu + f(_u, _v) + R * stim(i, j); 
       dv[idx] = d * DD * lapv + g(_u, _v);
     }
-  }
-  #pragma omp for collapse(2) private(i,j,up,down,left,right,idx,_u,_v,lapu,lapv) //interior points
-  for (i = 1; i < M-1; i++) {
-    for (j = 1; j < N-1; j++) {
-      idx = i * N + j;
-      down = (i - 1) * N + j;
-      up = (i + 1) * N + j;
-      left = i * N + j - 1;
-      right = i * N + j + 1;
-      _u = u[idx]; //create local copies
-      _v = v[idx];
-      lapu = u[up] + u[down] + u[left] + u[right] + -4.0 * _u; // discrete Laplacian
-      lapv = v[up] + v[down] + v[left] + v[right] + -4.0 * _v;
-      du[idx] = DD * lapu + f(_u, _v) + R * stim(i, j); // uses the differential equations to update du and dv
-      dv[idx] = d * DD * lapv + g(_u, _v);
+    #pragma omp for  //interior points
+    for (i = 1; i < M-1; i++) {
+      for (j = 1; j < N-1; j++) {
+        idx = i * N + j;
+        down = (i - 1) * N + j;
+        up = (i + 1) * N + j;
+        left = i * N + j - 1;
+        right = i * N + j + 1;
+        _u = u[idx]; //create local copies
+        _v = v[idx];
+        lapu = u[up] + u[down] + u[left] + u[right] + -4.0 * _u; // discrete Laplacian
+        lapv = v[up] + v[down] + v[left] + v[right] + -4.0 * _v;
+        du[idx] = DD * lapu + f(_u, _v) + R * stim(i, j); // uses the differential equations to update du and dv
+        dv[idx] = d * DD * lapv + g(_u, _v);
+      }
     }
   }
-}
-//should be straight forward to parallelise as there are no data dependencies
-//again similar to init, this could be collapsed but likely not worth it
-//also similar to init, i could condense this into one loop over the range of idx 
-
-/*unlike init, each step here does not require calculating i and j from idx 
-and so i believe this may yield performace gains*/
-void step(const double *du, const double *dv, double *u, double *v) {
-int idx,i,j;
-for (i = 0; i < M; i++) {
-  for (j = 0; j < N; j++) {
-    idx = i * N + j;
-    //for every grid point update u and v
-    u[idx] += dt * du[idx];
-    v[idx] += dt * dv[idx];
   }
-}
-}
-// calculate the norm of a 2D field stored in a 1D array
-// this can be parallellised using a reduction clause
-// this also might be worth condensing into a single loop over idx, similar to init and step
-double norm(const double *x) {
-double nrmx = 0.0;
-int idx,i,j;
-#pragma omp for shared(x,M,N) private(idx,i,j) reduction(+:nrmx)
-for (i = 0; i < M; i++) {
-  for (j = 0; j < N; j++) {
-    idx = i * N + j;
-    nrmx += x[idx] * x[idx];
-  }
-}
-return nrmx;
-}
 
+  //should be straight forward to parallelise as there are no data dependencies
+  //again similar to init, this could be collapsed but likely not worth it
+  //also similar to init, i could condense this into one loop over the range of idx 
+
+  /*unlike init, each step here does not require calculating i and j from idx 
+  and so i believe this may yield performace gains*/
+  void step(const double *du, const double *dv, double *u, double *v) {
+    int idx,i,j;
+    for (i = 0; i < M; i++) {
+      for (j = 0; j < N; j++) {
+        idx = i * N + j;
+      //for every grid point update u and v
+        u[idx] += dt * du[idx];
+        v[idx] += dt * dv[idx];
+      }
+    }
+  }
+  // calculate the norm of a 2D field stored in a 1D array
+  // this can be parallellised using a reduction clause
+  // this also might be worth condensing into a single loop over idx, similar to init and step
+  double norm(const double *x) {
+    double nrmx = 0.0;
+    int idx,i,j;
+    for (i = 0; i < M; i++) {
+      for (j = 0; j < N; j++) {
+        idx = i * N + j;
+        nrmx += x[idx] * x[idx];
+      }
+  }
+  return nrmx;
+}
 
 int main(int argc, char **argv) {
-double start,end;
-start = omp_get_wtime();
-double t = 0.0, nrmu, nrmv;
-int writeInd = 0;
-double stats[T / m][3];
+  double start,end;
+  start = omp_get_wtime();
+  double t = 0.0, nrmu, nrmv;
+  int writeInd = 0;
+  double stats[T / m][3];
 
-// Allocate memory for 1D arrays representing 2D grids
-double *u = (double *)malloc(M * N * sizeof(double));
-double *v = (double *)malloc(M * N * sizeof(double));
-double *du = (double *)malloc(M * N * sizeof(double));
-double *dv = (double *)malloc(M * N * sizeof(double));
+  // Allocate memory for 1D arrays representing 2D grids
+  double *u = (double *)malloc(M * N * sizeof(double));
+  double *v = (double *)malloc(M * N * sizeof(double));
+  double *du = (double *)malloc(M * N * sizeof(double));
+  double *dv = (double *)malloc(M * N * sizeof(double));
 
-// Check for allocation success
-if (!u || !v || !du || !dv) {
-  fprintf(stderr, "Error: Failed to allocate memory\n");
-  return 1;
-}
+  // Check for allocation success
+  if (!u || !v || !du || !dv) {
+    fprintf(stderr, "Error: Failed to allocate memory\n");
+    return 1;
+  }
 
-// initialize the state
-init(u, v);
-// time-loop
-#pragma omp parallel default(none) shared(u,v,du,dv,stats,t,nrmu,nrmv,writeInd)
-{
-for (int k = 0; k < T; k++) {
+  // initialize the state
+  init(u, v);
+  // time-loop
+  for (int k = 0; k < T; k++) {
     // track the time
     t = dt * k;
     // evaluate the PDE
     dxdt(du, dv, u, v);
     // update the state variables u,v
-    #pragma omp single{
-      step(du, dv, u, v);
-    }
+    step(du, dv, u, v);
     if (k % m == 0) { // every m time steps, store norms
       writeInd = k / m;
       // calculate the norms
@@ -181,10 +176,9 @@ for (int k = 0; k < T; k++) {
       stats[writeInd][2] = nrmv;
     }
   }
-  }
   // write norms output
-  char filename[30];
-  sprintf(filename, "%d_cores_part1.dat", omp_get_max_threads());
+  char filename[50];
+  sprintf(filename, "%d_cores_part1_dxdt_pllboundary.dat", omp_get_max_threads());
   FILE *fptr = fopen(filename, "w");
   fprintf(fptr, "#t\t\tnrmu\t\tnrmv\n");
   for (int k = 0; k < (T / m); k++) {

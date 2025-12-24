1abc6f3240861ad1fd28ff946801f990acf41ac5
Ciaran Reed <ciaranreed@hotmail.co.uk>
2025-12-24 15:38:36 +0000

    Removing global consts from shared scope

--- coursework/part1/src/part1_jointregion.c
+++ coursework/part1/src/part1_jointregion.c
@@ -15,7 +15,7 @@ However this requires doing two divisions every iteration so this may be detrime
 */
 void init(double *u, double *v) {
   int idx,i,j;
-  #pragma omp parallel for default(none) shared(u,v,N,M,uhi,ulo,vhi,vlo) private(idx,i,j) collapse(2)
+  #pragma omp parallel for default(none) shared(u,v) private(idx,i,j) collapse(2)
   for (i = 0;i<M;i++){
     for (j = 0;j<N;j++){
       idx = i * N + j;
@@ -123,7 +123,7 @@ void step(const double *du, const double *dv, double *u, double *v) {
 double norm(const double *x) {
   double nrmx = 0.0;
   int idx,i,j;
-  #pragma omp parallel for default(none) shared(x,N,M) private(i,j,idx) reduction(+:nrmx)
+  #pragma omp parallel for default(none) shared(x) private(i,j,idx) reduction(+:nrmx)
   for (i = 0; i < M; i++) {
     for (j = 0; j < N; j++) {
       idx = i * N + j;
@@ -156,7 +156,7 @@ int main(int argc, char **argv) {
   // initialize the state
   init(u, v);
   // time-loop
-  #pragma omp parallel default(none) shared(u,v,du,dv,stats,k,T,dt,t,m,nrmu,nrmv,writeInd,N,M,DD,R,d)
+  #pragma omp parallel default(none) shared(u,v,du,dv,stats,k,t,nrmu,nrmv,writeInd)
   {
     for (k = 0; k < T; k++) {
       // track the time

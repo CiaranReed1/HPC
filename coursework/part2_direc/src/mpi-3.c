4e41cdb336099ebfc5b816599260dcef4a635d81
Ciaran Reed <ciaranreed@hotmail.co.uk>
2025-12-31 11:58:41 +0000

    fixed output file name

--- coursework/part2_direc/src/mpi-3.c
+++ coursework/part2_direc/src/mpi-3.c
@@ -244,7 +244,7 @@ int main(int argc, char **argv) {
   // write norms output
   if (rank == 0){
     char filename[60];
-    sprintf(filename, "programData/mpi-2_problemsize-%d_%d-ranks.dat", problem_size, size);
+    sprintf(filename, "programData/mpi-3_problemsize-%d_%d-ranks.dat", problem_size, size);
     FILE *fptr = fopen(filename, "w");
     fprintf(fptr, "#t\t\tnrmu\t\tnrmv\n");
     for (int k = 0; k < (T / m); k++) {
